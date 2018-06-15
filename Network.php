776e60f25c4f6b35ec3e1d1b1e7f5980f6daea35
Bright Robert <guruskid@yahoo.com>
2018-06-15 15:24:33 +0100

    fix

--- Network.php
+++ Network.php
@@ -29,52 +29,55 @@
         <div class="container">
           <div class="brand">
             <h1 class="brand_name"><a href="./">INTELLITECH</a></h1><sup>&#174;</sup>
-            <p class="slogan">CONFIGURE | CREATE | INNOVATE</p>
+            <p class="slogan"><span class="sl">CONFIGURE</span> | <span class="sl">CREATE</span> | <span class="sl">INNOVATE</span></p>
           </div>
         </div>
         <div id="stuck_container" class="stuck_container">
           <div class="container">
-<nav class="nav">
-  <ul data-type="navbar" class="sf-menu">
-    <li><a href="./">Home</a></li>
-    <li class="active"><a href="Services">Services</a>
-      <ul>
-        <li><a href="Maintenance">MAINTENENCE</a></li>
-        <li><a href="Training">TRAINING</a></li>
-        <li><a href="Consultancy">CONSULTANCY</a></li>
-        <li><a href="Sales">SALES</a></li>
-        <li><a href="Network">NETWORK DESIGN</a>
-          <ul>
-            <li><a href="Security">SECURITY</a></li>
-          </ul>
-        </li>
-        <li><a href="Web">WEB &#38; SOFTWARE DEVELOPMENT</a></li>
-      </ul>
-    </li>
-    <li><a href="About">About</a></li>
-    <li><a href="Contacts">Contacts</a></li>
-  </ul>
-  <div class="sf-menu so">
-    <div class="social">
-      <a href=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
-    </div>
-    <div class="social">
-      <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
-    </div>
-    <div class="social">
-      <a href=""><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-    </div> 
-    <div class="social">
-      <a href=""><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
-    </div>    
-    <div class="social">
-      <a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-    </div>    
-    <div class="social">
-      <a href=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-    </div>
-  </div>
-</nav>
+            <nav class="nav">
+              <ul data-type="navbar" class="sf-menu">
+                <li class="active"><a href="./">Home</a></li>
+                <li><a href="Services">Services</a>
+                  <ul>
+                    <li><a href="Maintenance">MAINTENENCE</a></li>
+                    <li><a href="Training">TRAINING</a></li>
+                    <li><a href="Consultancy">CONSULTANCY</a></li>
+                    <li><a href="Sales">SALES</a></li>
+                    <li><a href="Network">NETWORK DESIGN</a>
+                      <ul>
+                        <li><a href="Security">SECURITY</a></li>
+                      </ul>
+                    </li>
+                    <li><a href="Web">WEB &#38; SOFTWARE DEVELOPMENT</a></li>
+                  </ul>
+                </li>
+                <li><a href="About">About</a></li>
+                <li><a href="Contacts">Contacts</a></li>
+              </ul>
+              <div class="sf-menu so">
+                <div class="social">
+                  <a href="" title="FACEBOOK"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                </div>
+                <div class="social">
+                  <a href="" title="TWITTER"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </div>
+                <div class="social">
+                  <a href="" title="GOOGLE PLUS"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+                </div>
+                <div class="social">
+                  <a href="" title="YOUTUBE"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                </div>
+                <div class="social">
+                  <a href="" target="_blank" title="INSTAGRAM"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </div>
+                <div class="social">
+                  <a href="" title="LINKEDIN"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                </div>
+                <div class="social">
+                  <a href="" title="GITHUB"><i class="fa fa-github" aria-hidden="true"></i></a>
+                </div>
+              </div>
+            </nav>
           </div>
         </div>
       </header>
