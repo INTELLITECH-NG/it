95a9a07efbf5bc412c75df3896e53f6996fc3057
Bright Robert <guruskid@yahoo.com>
2018-07-21 16:36:49 +0100

    Add

--- Blog.php
+++ Blog.php
@@ -1,3 +1,4 @@
+<?php include('inc/database.php') ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -8,6 +9,7 @@
     <link rel="icon" href="images/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/grid.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/publicstyle.css">
     <script src="js/jquery.js"></script>
     <script src="js/jquery-migrate-1.2.1.js"></script><!--[if lt IE 9]>
     <html class="lt-ie9">
@@ -26,61 +28,7 @@
       
       -->
       <header>
-        <div class="container">
-          <div class="brand">
-            <h1 class="brand_name"><a href="./">INTELLITECH</a></h1><sup>&#174;</sup>
-            <p class="slogan"><span class="sl">CONFIGURE</span> | <span class="sl">CREATE</span> | <span class="sl">INNOVATE</span></p>
-          </div>
-        </div>
-        <div id="stuck_container" class="stuck_container">
-          <div class="container">
-            <nav class="nav">
-              <ul data-type="navbar" class="sf-menu">
-                <li><a href="./">Home</a></li>
-                <li><a href="Services">Services</a>
-                  <ul>
-                    <li><a href="Maintenance">MAINTENENCE</a></li>
-                    <li><a href="Training">TRAINING</a></li>
-                    <li><a href="Consultancy">CONSULTANCY</a></li>
-                    <li><a href="Sales">SALES</a></li>
-                    <li><a href="Network">NETWORK DESIGN</a>
-                      <ul>
-                        <li><a href="Security">SECURITY</a></li>
-                      </ul>
-                    </li>
-                    <li><a href="Web">WEB &#38; SOFTWARE DEVELOPMENT</a></li>
-                  </ul>
-                </li>
-                <li class="active"><a href="Blog">Blog</a></li>
-                <li><a href="About">About</a></li>
-                <li><a href="Contacts">Contacts</a></li>
-              </ul>
-              <div class="sf-menu so">
-                <div class="social">
-                  <a href="" title="FACEBOOK"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                </div>
-                <div class="social">
-                  <a href="" title="TWITTER"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                </div>
-                <div class="social">
-                  <a href="" title="GOOGLE PLUS"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                </div>
-                <div class="social">
-                  <a href="" title="YOUTUBE"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
-                </div>
-                <div class="social">
-                  <a href="" target="_blank" title="INSTAGRAM"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-                <div class="social">
-                  <a href="" title="LINKEDIN"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-                </div>
-                <div class="social">
-                  <a href="" title="GITHUB"><i class="fa fa-github" aria-hidden="true"></i></a>
-                </div>
-              </div>
-            </nav>
-          </div>
-        </div>
+        <?php include('inc/header.php'); ?>
       </header>
       <!--
       ========================================================
@@ -88,9 +36,6 @@
       ========================================================
       -->
 	  <main>
-      <div class="container">
-        
-      </div>
 		</main>
       <!--
       ========================================================
@@ -98,12 +43,7 @@
       ========================================================
       -->
       <footer>
-        <section>
-          <div class="container">
-            <div class="copyright"><a href="http://www.intellitech.ng">INTELLITECH </a>© <span id="copyright-year"></span>.&nbsp;&nbsp;<a href="Privacy Policy">Privacy Policy</a>
-            </div>
-          </div>
-        </section>
+        <?php include("inc/footer.php"); ?>
       </footer>
     </div>
     <script src="js/script.js"></script>
---
+++ inc/footer.php
@@ -0,0 +1,6 @@
+        <section>
+          <div class="container">
+            <div class="copyright"><a href="http://www.intellitech.ng">INTELLITECH </a>© <span id="copyright-year"></span>.&nbsp;&nbsp;<a href="Privacy Policy">Privacy Policy</a>
+            </div>
+          </div>
+        </section>
