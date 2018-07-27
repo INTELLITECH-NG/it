75bdf67f9fd1298dfa04736341c976852e95f608
Bright Robert <guruskid@yahoo.com>
2018-07-27 21:53:18 +0100

    Add

--- Blog.php
+++ Blog.php
@@ -36,111 +36,51 @@
       ========================================================
       -->
 	  <main>
+     <section class="well1">
           <div class="container">
             <div class="row">
-              <!-- Blog Entries Column -->
-              <div class="col-md-8">
-                <h1 class="my-4">Page Heading
-                  <small>Secondary Text</small>
-                </h1>
-                <!-- Blog Post -->
-                <div class="card mb-4">
-                  <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                  <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    <p>Posted on January 1, 2017 by</p>
-                    <a href="#">Start Bootstrap</a>
-                  </div>
+              <div class="grid_8">
+                <img src="images/750x300.png" class="post" alt="">
+                <div class="left">
+                  <h2>Luching of New Software</h2>
                 </div>
-                <!-- Blog Post -->
-                <div class="card mb-4">
-                  <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                  <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    <p>Posted on January 1, 2017 by</p>
-                    <a href="#">Start Bootstrap</a>
-                  </div>
+                <div class="right">
+                  <p>Posted on January 1, 2017 by <a href="#">Admin </a></p>
                 </div>
-                <!-- Blog Post -->
-                <div class="card mb-4">
-                  <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
-                  <div class="card-body">
-                    <h2 class="card-title">Post Title</h2>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                    <a href="#" class="btn btn-primary">Read More &rarr;</a>
-                  </div>
-                  <div class="card-footer text-muted">
-                    <p>Posted on January 1, 2017 by</p>
-                    <a href="#">Start Bootstrap</a>
-                  </div>
-                </div>
-                <!-- Pagination -->
-                <ul class="pagination justify-content-center mb-4">
-                  <li class="page-item">
-                    <a class="page-link" href="#">&larr; Older</a>
-                  </li>
-                  <li class="page-item disabled">
-                    <a class="page-link" href="#">Newer &rarr;</a>
-                  </li>
-                </ul>
+                <div class="clear"></div>
+                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatu. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p><a href="#" class="btn">Read more</a>
               </div>
-              <!-- Sidebar Widgets Column -->
-              <div class="col-md-4">
-                <!-- Search Widget -->
-                <div class="card my-4">
-                  <h5 class="card-header">Search</h5>
-                  <div class="card-body">
-                    <div class="input-group">
-                      <input type="text" class="form-control" placeholder="Search for...">
-                      <span class="input-group-btn">
-                        <button class="btn btn-secondary" type="button">Go!</button>
-                      </span>
-                    </div>
+              <div class="grid_4">
+                <div class="info-box">
+                  <h2>Help center</h2>
+                  <hr>
+                  <h3>Category:</h3>
+                  <div class="clear"></div>
+                  <div>
+                    <ul>
+                    <?php 
+                    $query = "SELECT * FROM categories";
+                    $categories_query = mysqli_query($conn, $query);
+
+                    while ($row = mysqli_fetch_assoc($categories_query)) {
+                         $cat_title = $row['title'];
+
+                         echo "<li><a href='#'>{$cat_title}</a></li>";
+                     } ?>
+                   </ul>
                   </div>
-                </div>
-                <!-- Categories Widget -->
-                <div class="card my-4">
-                  <h5 class="card-header">Categories</h5>
-                  <div class="card-body">
-                    <div class="row">
-                      <div class="col-lg-6">
-                        <ul class="list-unstyled mb-0">
-                          <li><a href="#">Web Design</a></li>
-                          <li><a href="#">HTML</a></li>
-                          <li><a href="#">Freebies</a></li>
-                        </ul>
-                      </div>
-                      <div class="col-lg-6">
-                        <ul class="list-unstyled mb-0">
-                          <li><a href="#">JavaScript</a></li>
-                          <li><a href="#">CSS</a></li>
-                          <li><a href="#">Tutorials</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <!-- Side Widget -->
-                <div class="card my-4">
-                  <h5 class="card-header">Side Widget</h5>
-                  <div class="card-body">
-                    <p>You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!</p>
-                  </div>
+                  <a href="" target=""></a>
+                  <hr>
+                  <h3>24/7 Online Support:</h3>
+                  <dl>
+                    <dt>800-2345-6789</dt>
+                  </dl>
                 </div>
               </div>
             </div>
-            <!-- /.row -->
           </div>
-          <!-- /.container -->
-        </main>
+        </section> 
+    </main>
       <!--
       ========================================================
                                   FOOTER
