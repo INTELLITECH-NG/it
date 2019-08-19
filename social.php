2bafeaef6fff7938b3350af4f0aba8ffa504829f
Bright Robert <guruskid@yahoo.com>
2019-08-19 10:36:45 +0100

    Social

--- social.php
+++ social.php
@@ -1,17 +1,17 @@
 <div class="sf-menu so">
-  <div class="social">
+  <div class="social facebook">
     <a href="" title="intellitechng"><i class="fa fa-facebook" aria-hidden="true"></i></a>
   </div>
-  <div class="social">
+  <div class="social twitter">
     <a href="" title="intellitechng"><i class="fa fa-twitter" aria-hidden="true"></i></a>
   </div>
-  <div class="social">
+  <div class="social instagram">
     <a href="" target="_blank" title="intellitechng"><i class="fa fa-instagram" aria-hidden="true"></i></a>
   </div>
-  <div class="social">
+  <div class="social linkedin">
     <a href="" title="LINKEDIN"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
   </div>
-  <div class="social">
+  <div class="social youtube">
     <a href="" title="YOUTUBE"><i class="fa fa-youtube" aria-hidden="true"></i></a>
   </div>
 </div>
