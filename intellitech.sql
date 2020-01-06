df198dcb3259e6b30fd8a9cfa9af1d58dc98b491
Bright Robert <guruskid@yahoo.com>
2020-01-06 09:16:52 +0100

    Database

--- intellitech.sql
+++ intellitech.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 07, 2019 at 09:12 PM
+-- Generation Time: Jan 06, 2020 at 09:09 AM
 -- Server version: 10.1.31-MariaDB
 -- PHP Version: 7.2.3
 
@@ -33,15 +33,6 @@ CREATE TABLE `categories` (
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `title`) VALUES
-(1, 'Trending'),
-(2, 'Javascript'),
-(3, 'Programming');
-
 -- --------------------------------------------------------
 
 --
@@ -58,14 +49,21 @@ CREATE TABLE `comment` (
   `status` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `comment`
+-- Table structure for table `intern`
 --
 
-INSERT INTO `comment` (`id`, `post`, `date`, `author`, `email`, `comment`, `status`) VALUES
-(3, 69, '2019-07-15', 'Bright Robert', 'configureall@gmail.com', 'njfngrjnknjdnvjdnjndgj', 'Approved'),
-(4, 69, '2019-07-15', 'Bright Robert', 'configureall@gmail.com', 'njfngrjnknjdnvjdnjndgj', 'Approved'),
-(5, 74, '2019-07-17', 'Nelson', 'configureall@gmail.com', 'How are u', 'Unapproved');
+CREATE TABLE `intern` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `track` varchar(255) NOT NULL,
+  `level` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -87,17 +85,6 @@ CREATE TABLE `post` (
   `view_count` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `post`
---
-
-INSERT INTO `post` (`id`, `category`, `title`, `author`, `date`, `image`, `content`, `tags`, `comment_count`, `status`, `view_count`) VALUES
-(69, 2, 'UPGRADE', 'Guru Robert', '2019-07-15', 'slider-image-1.jpg', '<p>kniv kdsvnkfnbclkmkdfvmkvm kvirkm vnrigivldkclmv ldcof</p>', 'updrade', '', 'Published', 58),
-(70, 2, 'NEXT', 'Guru Robert', '2019-07-15', '70074128127320.jpg', '<p>DBJSNKDNCKSNKM</p>', '#Bight', '', 'Published', 5),
-(71, 2, 'Technologies', 'Guru Robert', '2019-07-15', 'slider-image-2.jpg', '<p>JDCJDNKDCLKOjifkvndfd;</p>', '#Wizard #Nice three', '', 'Draft', 3),
-(72, 2, 'FORWARD', 'Guru Robert', '2019-07-15', '69958382618900.jpg', '<p>esdcokodmckdj clslspfvodkvlmdkckdmmlmkfmkvv</p>', '#Wizard #Nice three', '', 'Draft', 10),
-(73, 2, 'DOWNGRADE', 'Guru Robert', '2019-07-15', '70329315134960.jpg', '<p>JFHKVNVIJFKVFDMNKVNFNVKF SKMSC CMDPKER</p>', '#Wizard #Nice One', '', 'Draft', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -118,31 +105,6 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `firstname`, `lastname`, `email`, `image`, `role`, `randSalt`, `date`) VALUES
-(11, 'codengr', '$1$TRFE4BWN$yhc1urEV4q5sIwSvpukGi.', 'Bright', 'Robert', 'configureall@gmail.com', '', 'Subscriber', '', '2019-06-28'),
-(12, 'virus', '$1$1ZU6EVIR$Pa/x.jhe1juy8TCjupWOH.', 'Guru', 'Robert', 'bright@intellitech.ng', '', 'Admin', '', '2019-06-28'),
-(13, 'bright', 'Kingofpop', 'Bright', 'robert', 'bright.robert@intellitech.ng', '', 'Admin', '', '2019-06-06'),
-(24, 'man', 'Kingofpop', 'Bright', 'Robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$iamtoogoodinprogramming', '2019-06-28'),
-(25, 'guru', '$1$wr33JAaW$.noPM44aoFzqVdtVmOdtE/', 'Bright', 'Robert', 'bright@intellitech.ng', '', 'Subscriber', '$2y$10$iamtoogoodinprogramming', '2019-06-28'),
-(26, 'glory', '$1$HVot3ipq$spkhHl4ot.KVHIZ78vTes/', 'Glory', 'Bassey', 'configureall@gmail.com', '', 'Subcriber', '$2y$10$iamtoogoodinprogramming', '2019-06-28'),
-(27, 'wow', '$1$A89Ffsj2$Q4UEvZJwZ4caInW5fTBg9/', 'bright', 'robert', 'bright@intellitech.ng', '', 'Subscriber', '$2y$10$iamtoogoodinprogramming', '2019-06-28'),
-(28, 'name', '$1$lzExuY5S$rM/DwxJT5ce8vTbwSDYYw/', 'Bright', 'nelseon', 'bright@intellitech.ng', '', 'Admin', '$2y$10$iamtoogoodinprogramming', '2019-06-29'),
-(29, 'glory', '$2y$12$2BdEjMUqv7za4DV0NxCUqOk3dNog3edQuksx3kT13imcG3PAWySlW', 'Glory', 'Robert', 'glory.angela@gmail.com', '', 'Admin', '$2y$10$programmingguru', '2019-07-06'),
-(30, 'guru', '$2y$12$T70mBD4b7uhjQd3w1SzTDuQ2DjDQ.InpY6R2kkwR5gQ4V.b2mJKkq', 'Bright', 'Robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(31, 'guru', '$2y$12$xeQrI0wkgUSxYI4pHeyVRe027nRtIPPad2WZIqec4tFoXf7CrGhKS', 'bright', 'guruskid', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(32, 'guru', '$2y$12$/nKoQyJpof70q8COO0kdjONBmRBHiHkpK/bgdIvawulqB.fZAO4xe', 'Bright', 'robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(33, 'guru', '$2y$12$ofq1cndx//hZ5sdPOKTw..fQXazmvD7IhqpsAtTSJakxkOxC0oKFu', 'Bright', 'robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(34, 'error', '$2y$12$i1l19fQ.JAeKNrhC0D49yO2fmlF5JZOw6D.e1qpN/OXhg3HPrFkeC', 'Bright', 'robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(35, 'wewe', '$2y$12$bIV6n4yzSG76tZUBCzfSbeIe3qi/IicP5/w/HcVQAsELf6QajWepe', 'Bright', 'Ronert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(36, 'wewee', '$2y$12$PuGd//l.kBrgl4SnuVey6eZUQFshfDHEaZj6OE2NDgAReNJultcha', 'Bright', 'Robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(37, 'wooooooooooow', '$2y$12$CCi4Us.lZPBcGuGqfwPcMOFmcTp.dHGtybZKl9oWcgVEdmWaETmga', 'Bright', 'robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(38, 'wooow', '$2y$12$tPu9h7UrtqFGn8ozZJFTnOgBNohwFFPwCi5I53KhG646LyfMvM1ve', 'Bright', 'Robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23'),
-(39, 'king', '$2y$12$cvcNIrZclVbaOfnUKDL.RuO/dpifeRybs1c2WkWE8fsvFwCmBBojO', 'Bright', 'robert', 'configureall@gmail.com', '', 'Subscriber', '$2y$10$programmingguru', '2019-07-23');
-
---
 -- Indexes for dumped tables
 --
 
@@ -156,6 +118,12 @@ ALTER TABLE `categories`
 -- Indexes for table `comment`
 --
 ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `intern`
+--
+ALTER TABLE `intern`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -178,25 +146,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `intern`
+--
+ALTER TABLE `intern`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
