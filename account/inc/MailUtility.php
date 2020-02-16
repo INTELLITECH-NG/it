0baa47486eda31266388ecdac6ccd85306d61106
Bright Robert <guruskid@yahoo.com>
2020-02-16 14:34:50 +0100

    Update

--- account/inc/MailUtility.php
+++ account/inc/MailUtility.php
@@ -24,22 +24,34 @@ class MailUtility {
     function sendMail($toList, $subject, $message) {
         if(is_array($toList)){
             foreach ($toList as $to) {
-                $this->mail->AddBCC(trim($to));
+
+                $this->mail->AddAddress($to);
+                $this->mail->Subject = $subject;
+                $this->mail->WordWrap = 80;
+                $this->mail->MsgHTML($message);
+                $this->mail->IsHTML(true);
+
+                if (!$this->mail->Send())
+                {
+                  echo "Couldn't send to {$to}\n";
+                }else {
+                  echo "sent Successfuly to {$to}\n";
+                }
             }
         }else{
-            $this->mail->AddBcc(trim($toList));
+            $this->mail->AddAddress($toList);
+            $this->mail->Subject = $subject;
+            $this->mail->WordWrap = 80;
+            $this->mail->MsgHTML($message);
+            $this->mail->IsHTML(true);
+            if (!$this->mail->Send()){
+              echo "sent successfuly to {$to}\n";
+            }else{
+              echo "Couldn't send to {$to}\n";
+            }
         }
-        
-        $this->mail->Subject = $subject;
-        $this->mail->WordWrap = 80;
-        $this->mail->MsgHTML($message);
-        $this->mail->IsHTML(true);
-        if (!$this->mail->Send())
-            return FALSE;
-        else
-            return TRUE;;
+
+        return TRUE;
     }
 
 }
-
-?>
